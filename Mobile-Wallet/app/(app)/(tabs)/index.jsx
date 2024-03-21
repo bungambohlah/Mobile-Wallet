3e1f68989b83df0d19c53be01f3f65c371337d74
alfset <alfset5@gmail.com>
2024-03-22 01:09:41 +0700

    revert update

--- Mobile-Wallet/app/(app)/(tabs)/index.jsx
+++ Mobile-Wallet/app/(app)/(tabs)/index.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { CoinList } from "../../../components/card/CoinList";
-import { StyledView } from "../../../constants/styledComponents"
-import { CardView } from '../../../components/card/CardView';
-import { trendingCoins } from '../../../constants/trendingCoins'
-import { themeColor } from '../../../constants/themeColor';
-import { Heading } from '../../../components/typography/Heading';
+import { View, Text, StyleSheet } from "react-native";
 
-
+import { themeColor } from "../../../constants/themeColor";
 
 export default function Tab() {
   return (
-    <StyledView className="container min-h-screen justify-center items-start px-5 pt-16 pb-20 space-y-2" style={{ backgroundColor: themeColor.appBackgroundColor }}>
-      <CardView title={`Welcome, Alfin Sugestian`} subtitle={`Make your investment today`} buttonText={`Invest Today`} onPress={() => console.log("Button pressed")} />
-      <Heading title={`Trending Coins`} fontSize="xl" setButton={false} />
-      <CoinList list={trendingCoins} isArrow={false} />
-    </StyledView >
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themeColor.appBackgroundColor,
+    color: "white",
+    fontFamily: "Inter_400Regular",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Inter_400Regular",
+    color: "white",
+  },
+});
